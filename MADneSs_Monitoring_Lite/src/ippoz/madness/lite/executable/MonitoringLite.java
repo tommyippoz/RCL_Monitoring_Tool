53c64ed272a0d36d6b9130e6e4f8f56febd7e850
Tommaso Zoppi <tommaso.zoppi@unifi.it>
2017-03-24 12:50:46 +0100

    First Push

---
+++ MADneSs_Monitoring_Lite/src/ippoz/madness/lite/executable/MonitoringLite.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ippoz.madness.lite.executable;
+
+/**
+ * @author Tommy
+ *
+ */
+public class MonitoringLite {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
